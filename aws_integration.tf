066924ccbbc568026e2dbbe9d2531917be6cf572
eminalemdar <emnalmdr@gmail.com>
2023-05-10 14:17:41 +0300

    updated AWS config

--- aws_integration.tf
+++ aws_integration.tf
@@ -9,7 +9,6 @@ resource "spacelift_aws_integration" "aws" {
   # We need to set the ARN manually rather than referencing the role to avoid a circular dependency
   role_arn                       = local.role_arn
   generate_credentials_in_worker = true
-  space_id = "demo-space"
   depends_on = [
     aws_iam_role.spacelift_role
   ]
